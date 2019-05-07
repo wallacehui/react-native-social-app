2007698705df0d5e5d867f4181de11b4de9e15ae
Wallace Hui <wallhuiwallace@gmail.com>
2019-05-08 05:35:13 +0800

    Fetch and obtain users data model in screen

--- socialApp/src/redux/reducers/user.ts
+++ socialApp/src/redux/reducers/user.ts
@@ -5,10 +5,12 @@ import produce from "immer";
 
 export interface State {
   userMap: { [id: number]: User };
+  userIds: number[];
 }
 
 const initialState: State = {
   userMap: {},
+  userIds: [],
 };
 
 export default function reducer(
@@ -18,11 +20,24 @@ export default function reducer(
   return produce(state, draft => {
     switch (action.type) {
       case "DidFetchUserListAction": {
-        for (const user of action.payload.users) {
+        const users = action.payload.users;
+        draft.userIds = users.map(user => user.id);
+        for (const user of users) {
           draft.userMap[user.id] = user;
         }
         break;
       }
     }
   });
+}
+
+export function selectUsers(state: ReduxState): User[] {
+  const users: User[] = [];
+  for (const id of state.user.userIds) {
+    const user = state.user.userMap[id];
+    if (user) {
+      users.push(user);
+    }
+  }
+  return users;
 }
--- socialApp/src/screens/UserListScreen.tsx
+++ socialApp/src/screens/UserListScreen.tsx
@@ -1,5 +1,11 @@
 import * as React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Dispatch, ReduxState } from "../redux/store";
+import { bindActionCreators } from "redux";
+import { fetchUsers } from "../redux/actions/user";
+import { connect } from "react-redux";
+import { selectUsers } from "../redux/reducers/user";
+import { User } from "../models/user";
 
 const styles = StyleSheet.create({
   rootContainer: {
@@ -13,8 +19,31 @@ const styles = StyleSheet.create({
   },
 });
 
-export default class UserListScreen extends React.PureComponent {
+function mapStateToProps(state: ReduxState) {
+  return {
+    users: selectUsers(state),
+  };
+}
+
+function mapDispatchToProps(dispatch: Dispatch) {
+  return {
+    userAction: bindActionCreators({ fetchUsers }, dispatch),
+  };
+}
+
+interface MapProps {
+  users: User[];
+}
+
+type Props = MapProps & ReturnType<typeof mapDispatchToProps>;
+
+export class UserListScreen extends React.PureComponent<Props> {
+  componentDidMount() {
+    this.props.userAction.fetchUsers();
+  }
+
   render() {
+    console.log("props data", this.props.users);
     return (
       <View style={styles.rootContainer}>
         <Text style={styles.dummyTitle}>User list screen</Text>
@@ -22,3 +51,8 @@ export default class UserListScreen extends React.PureComponent {
     );
   }
 }
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UserListScreen);
