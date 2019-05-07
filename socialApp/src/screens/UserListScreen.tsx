7607f3ad09d1ed758e6cb16b68e45feaa74cf98f
Wallace Hui <wallhuiwallace@gmail.com>
2019-05-08 06:00:57 +0800

    Bind user list into FlatList with simple layout

--- socialApp/src/screens/UserListScreen.tsx
+++ socialApp/src/screens/UserListScreen.tsx
@@ -6,18 +6,39 @@ import { fetchUsers } from "../redux/actions/user";
 import { connect } from "react-redux";
 import { selectUsers } from "../redux/reducers/user";
 import { User } from "../models/user";
+import { FlatList } from "react-native-gesture-handler";
 
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    backgroundColor: "red",
-    justifyContent: "center",
   },
-  dummyTitle: {
-    textAlign: "center",
+  cellContainer: {
+    flexDirection: "column",
+  },
+  userName: {
+    fontSize: 22,
     color: "#333333",
+    flex: 1,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+  },
+  separator: {
+    backgroundColor: "#E0E0E0",
+    marginLeft: 15,
+    height: 1,
+    flex: 1,
   },
 });
+
+function UserViewCell(props: { user: User }) {
+  const { user } = props;
+  return (
+    <View style={styles.cellContainer}>
+      <Text style={styles.userName}>{user.name}</Text>
+      <View style={styles.separator} />
+    </View>
+  );
+}
 
 function mapStateToProps(state: ReduxState) {
   return {
@@ -42,11 +63,23 @@ export class UserListScreen extends React.PureComponent<Props> {
     this.props.userAction.fetchUsers();
   }
 
+  keyExtractor = (item: User) => {
+    return item.id.toString();
+  };
+
+  renderItem = ({ item }: { item: User }) => {
+    return <UserViewCell user={item} />;
+  };
+
   render() {
-    console.log("props data", this.props.users);
+    const { users } = this.props;
     return (
       <View style={styles.rootContainer}>
-        <Text style={styles.dummyTitle}>User list screen</Text>
+        <FlatList
+          data={users}
+          renderItem={this.renderItem}
+          keyExtractor={this.keyExtractor}
+        />
       </View>
     );
   }
