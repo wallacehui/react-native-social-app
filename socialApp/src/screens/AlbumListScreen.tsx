0ee754c4960c2a486a6121b1da06b92408a9c04f
Wallace Hui <wallhuiwallace@gmail.com>
2019-05-09 05:05:29 +0800

    Bind fetched albums data into FlatList

--- socialApp/src/screens/AlbumListScreen.tsx
+++ socialApp/src/screens/AlbumListScreen.tsx
@@ -1,10 +1,18 @@
 import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import {
   NavigationScreenProps,
   NavigationScreenOptions,
+  FlatList,
 } from "react-navigation";
 import { User } from "../models/user";
+import { ReduxState, Dispatch } from "../redux/store";
+import { selectalbums } from "../redux/reducers/album";
+import { bindActionCreators } from "redux";
+import { fetchalbumsByUser } from "../redux/actions/album";
+import { connect } from "react-redux";
+import { Album } from "../models/album";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const styles = StyleSheet.create({
   rootContainer: {
@@ -14,6 +22,26 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: "#333333",
     textAlign: "center",
+  },
+  albumCellContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  albumTitle: {
+    fontSize: 18,
+    color: "#333333",
+    flex: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+  },
+  rightArrow: {
+    width: 30,
+    height: 30,
+  },
+  separator: {
+    backgroundColor: "#E0E0E0",
+    height: 1,
+    flex: 1,
   },
 });
 
@@ -25,9 +53,45 @@ interface State {
   user: User;
 }
 
-type Props = NavigationScreenProps<RouteProps>;
+interface MapProps {
+  albums: Album[];
+}
 
-export default class AlbumListScreen extends React.PureComponent<Props, State> {
+type Props = NavigationScreenProps<RouteProps> &
+  MapProps &
+  ReturnType<typeof mapDispatchToProps>;
+
+function mapStateToProps(state: ReduxState) {
+  return {
+    albums: selectalbums(state),
+  };
+}
+
+function mapDispatchToProps(dispatch: Dispatch) {
+  return {
+    albumAction: bindActionCreators({ fetchalbumsByUser }, dispatch),
+  };
+}
+
+function AlbumViewCell(props: { album: Album; onPress: () => void }) {
+  return (
+    <>
+      <TouchableOpacity
+        style={styles.albumCellContainer}
+        onPress={props.onPress}
+      >
+        <Text style={styles.albumTitle}>{props.album.title}</Text>
+        <Image
+          style={styles.rightArrow}
+          source={require("../resources/images/navigation_right_arrow.png")}
+        />
+      </TouchableOpacity>
+      <View style={styles.separator} />
+    </>
+  );
+}
+
+class AlbumListScreen extends React.PureComponent<Props, State> {
   static navigationOptions: NavigationScreenOptions = {
     headerTitle: <Text style={styles.title}>Albums</Text>,
   };
@@ -39,7 +103,40 @@ export default class AlbumListScreen extends React.PureComponent<Props, State> {
     };
   }
 
+  componentDidMount() {
+    this.props.albumAction.fetchalbumsByUser(this.state.user);
+  }
+
+  onPressAlbum = (album: Album) => {
+    // TODO: Navigate to photos screen
+  };
+
+  keyExtractor = (item: Album) => {
+    return item.id.toString();
+  };
+
+  renderItem = ({ item }: { item: Album }) => {
+    // tslint:disable-next-line:jsx-no-lambda
+    return (
+      <AlbumViewCell album={item} onPress={() => this.onPressAlbum(item)} />
+    );
+  };
+
   render() {
-    return <View style={styles.rootContainer} />;
+    const { albums } = this.props;
+    return (
+      <View style={styles.rootContainer}>
+        <FlatList
+          data={albums}
+          renderItem={this.renderItem}
+          keyExtractor={this.keyExtractor}
+        />
+      </View>
+    );
   }
 }
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AlbumListScreen);
