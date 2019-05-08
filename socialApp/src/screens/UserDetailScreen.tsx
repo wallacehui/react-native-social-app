ef2384984ff877a5913eab87d4ba115e4805f64e
Wallace Hui <wallhuiwallace@gmail.com>
2019-05-09 00:46:50 +0800

    Render user basic information with UI layout

--- socialApp/src/screens/UserDetailScreen.tsx
+++ socialApp/src/screens/UserDetailScreen.tsx
@@ -1,12 +1,54 @@
 import * as React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text, Image } from "react-native";
 import { User } from "../models/user";
-import { NavigationScreenProps } from "react-navigation";
+import {
+  NavigationScreenProps,
+  NavigationScreenOptions,
+} from "react-navigation";
+import { ScrollView } from "react-native-gesture-handler";
 
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    backgroundColor: "yellow",
+  },
+  title: {
+    fontSize: 18,
+    color: "#333333",
+    textAlign: "center",
+  },
+  detailContainer: {
+    flexDirection: "column",
+    marginVertical: 15,
+    paddingHorizontal: 15,
+  },
+  profileImage: {
+    width: 75,
+    height: 75,
+    alignSelf: "center",
+  },
+  name: {
+    fontSize: 18,
+    color: "#333333",
+    textAlign: "center",
+    flex: 1,
+    fontWeight: "bold",
+    marginVertical: 10,
+  },
+  infoRow: {
+    flex: 1,
+    flexDirection: "row",
+    marginVertical: 5,
+  },
+  infoRowLabel: {
+    fontSize: 18,
+    color: "#333333",
+    minWidth: 100,
+  },
+  infoRowValue: {
+    fontSize: 18,
+    color: "#333333",
+    flex: 1,
+    textAlign: "right",
   },
 });
 
@@ -16,8 +58,45 @@ export interface RouteProps {
 
 type Props = NavigationScreenProps<RouteProps>;
 
+function InfoRow(props: { label: string; value: string }) {
+  return (
+    <View style={styles.infoRow}>
+      <Text style={styles.infoRowLabel}>{props.label}</Text>
+      <Text style={styles.infoRowValue}>{props.value}</Text>
+    </View>
+  );
+}
+
 export default class UserDetailScreen extends React.PureComponent<Props> {
+  static navigationOptions = ({
+    navigation,
+  }: NavigationScreenProps<RouteProps>): NavigationScreenOptions => ({
+    headerTitle: (
+      <Text style={styles.title}>{navigation.getParam("user").username}</Text>
+    ),
+  });
+
+  renderUserInfo(user: User) {
+    return (
+      <View style={styles.detailContainer}>
+        <Image
+          style={styles.profileImage}
+          source={require("../resources/images/user_profile_icon.png")}
+        />
+        <Text style={styles.name}>{user.name}</Text>
+        <InfoRow label="Email:" value={user.email} />
+        <InfoRow label="Tel:" value={user.phone} />
+        <InfoRow label="Website:" value={user.website} />
+      </View>
+    );
+  }
+
   render() {
-    return <View style={styles.rootContainer} />;
+    const user = this.props.navigation.getParam("user");
+    return (
+      <ScrollView style={styles.rootContainer}>
+        {this.renderUserInfo(user)}
+      </ScrollView>
+    );
   }
 }
